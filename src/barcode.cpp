a2fcea93a0990af248fe320fe66c0a3c2ba676fe
Florian <luethifl@students.zhaw.ch>
2013-06-08 17:20:38 +0200

    sdkfj

---
+++ src/barcode.cpp
@@ -0,0 +1,9 @@
+/*
+ * barcode.cpp
+ *
+ *  Created on: Jun 8, 2013
+ *      Author: florian
+ */
+
+#include "barcode.h"
+
---
+++ src/barcode.h
@@ -0,0 +1,10 @@
+#ifndef BARCODE_H_
+#define BARCODE_H_
+
+class bar_code {
+
+
+
+};
+
+#endif /* BARCODE_H_ */
